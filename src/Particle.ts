ee279eaf7ffe4543a050206627d4798c6a815978
luwenxull <992273307@qq.com>
2018-07-25 22:33:28 +0800

    Particle

---
+++ src/Particle.ts
@@ -0,0 +1,69 @@
+import { Vector3 } from "three";
+
+export interface IParticle {
+  position: Vector3 // 位置
+  a: Vector3 // 加速度
+  v: Vector3 // 速度
+  mass: number
+}
+
+export const DRAG = 1 - 0.03
+export const TIME_STEP = 18 / 1000
+
+export class Particle implements IParticle {
+  private _a: Vector3 = new Vector3()
+  private _v: Vector3 = new Vector3()
+  constructor(
+    public position: Vector3 = new Vector3(),
+    public a: Vector3 = new Vector3(),
+    public v: Vector3 = new Vector3(),
+    public mass: number = 1,
+  ) {}
+
+  /**
+   * 将position，a，v复制到目标对象上
+   *
+   * @param {{
+   *     [prop: string]: any
+   *   }} target
+   * @returns {Particle}
+   * @memberof Particle
+   */
+  public copyTo(target: {
+    [prop: string]: any
+  }): Particle {
+    target.position = this.position
+    target.a = this.a
+    target.v = this.v
+    return this
+  }
+
+  /**
+   * 模拟运动
+   *
+   * @returns {Particle}
+   * @memberof Particle
+   */
+  public simulate(): Particle {
+    this.v
+      .multiplyScalar(DRAG) // 阻力
+      .add(this._a.copy(this.a).multiplyScalar(TIME_STEP)) // 更新速度
+    this.position
+      .add(this._v.copy(this.v).multiplyScalar(TIME_STEP))
+    this.a.set(0, 0, 0) // 加速度归零
+    return this
+  }
+
+  /**
+   * 施加力
+   * 最终会转换成加速度
+   *
+   * @param {Vector3} force
+   * @returns {Particle}
+   * @memberof Particle
+   */
+  public addForce(force: Vector3): Particle {
+    this.a.add(new Vector3().copy(force.multiplyScalar(1 / this.mass)))
+    return this
+  }
+}
---
+++ src/__tests__/Particle.ts
@@ -0,0 +1,26 @@
+import { Vector3 } from 'three'
+import { Particle, DRAG, TIME_STEP } from '../Particle'
+
+test('constructor', () => {
+  const p = new Particle()
+  expect(p.position.toArray()).toEqual([0, 0, 0])
+  expect(p.v.toArray()).toEqual([0, 0, 0])
+  expect(p.a.toArray()).toEqual([0, 0, 0])
+  expect(p.mass).toBe(1)
+})
+
+test('addForce', () => {
+  const p = new Particle()
+  expect(p.addForce(new Vector3(1, 1, 0)).a.toArray()).toEqual([1, 1, 0])
+  expect(p.addForce(new Vector3(1, -1, 0)).a.toArray()).toEqual([2, 0, 0])
+})
+
+test('simulate', () => {
+  const p = new Particle()
+  p.addForce(new Vector3(1, 1, 0)).simulate()
+  expect(p.v.toArray()).toEqual([TIME_STEP, TIME_STEP, 0])
+  expect(p.position.toArray()).toEqual([TIME_STEP ** 2, TIME_STEP ** 2, 0])
+  expect(p.a.toArray()).toEqual([0, 0, 0])
+  p.simulate()
+  expect(p.v.toArray()).toEqual([TIME_STEP * DRAG, TIME_STEP * DRAG, 0])
+})
